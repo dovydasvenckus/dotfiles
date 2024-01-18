20cbb7262ba532050a7265d635ebbf46b16420e4
Dovydas Venckus <dovydas.venckus@live.com>
2024-01-18 21:19:07 +0200

    Disable arrow keys

--- lua/dovydas/remap.lua
+++ lua/dovydas/remap.lua
@@ -1,3 +1,8 @@
+vim.keymap.set('n', '<Up>', '<Nop>')
+vim.keymap.set('n', '<Down>', '<Nop>')
+vim.keymap.set('n', '<Left>', '<Nop>')
+vim.keymap.set('n', '<Right>', '<Nop>')
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
