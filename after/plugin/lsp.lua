142b30ffe0d9add4d5bad8ef7b68a692432d3a3a
Dovydas Venckus <dovydas.venckus@live.com>
2024-01-20 19:25:54 +0200

    Basic LSP config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,16 @@
-require('lsp-zero')
-require('lspconfig').lua_ls.setup({})
+local lsp_zero = require('lsp-zero')
 
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+--- if you want to know more about lsp-zero and mason.nvim
+--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
