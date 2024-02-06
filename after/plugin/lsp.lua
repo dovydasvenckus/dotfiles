6b6f2717d516e1853411e47094b41cd0a3ed0000
Dovydas Venckus <dovydas.venckus@live.com>
2024-02-06 23:34:02 +0200

    Ensure that most used languages lsp servers are installed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,16 +1,17 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 --- if you want to know more about lsp-zero and mason.nvim
 --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  handlers = {
-    lsp_zero.default_setup,
-  },
+    ensure_installed = { 'bashls', 'cssls', 'dockerls', 'docker_compose_language_service', 'gopls', 'gradle_ls', 'html', 'jsonls', 'jdtls', 'lua_ls', 'marksman', 'lemminx', 'yamlls', 'tsserver' },
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
