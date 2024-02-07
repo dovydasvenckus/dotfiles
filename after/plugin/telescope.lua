d1d05173cd005e2ac16ab4fd034cab9af5f049e5
Dovydas Venckus <dovydas.venckus@live.com>
2024-02-07 18:02:28 +0200

    Truncate pathes for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,8 @@
+require('telescope').setup({
+    defaults = {
+        path_display = { "smart" }
+    }
+});
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
